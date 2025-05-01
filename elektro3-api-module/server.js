e815a4033ed47f05470ab52891e8632bbc71b286
Valter Rosa <valter@valterosa.com>
2025-05-01 01:02:39 +0100

    resolver o erro 500

--- elektro3-api-module/server.js
+++ elektro3-api-module/server.js
@@ -3,7 +3,7 @@ import { createRequestHandler } from "@vercel/remix/server";
 import * as build from "./build/server/index.js";
 
 // Certifique-se de definir o ambiente correto
-const mode = process.env.NODE_ENV;
+const mode = process.env.NODE_ENV || "production";
 
 export default createRequestHandler({
   build,
@@ -11,6 +11,12 @@ export default createRequestHandler({
   getLoadContext(req, res) {
     return {
       env: process.env,
+      waitUntil: (promise) => {
+        // Esta função é necessária para o Vercel Edge Runtime
+        if (req.waitUntil) {
+          req.waitUntil(promise);
+        }
+      },
     };
   },
 });
