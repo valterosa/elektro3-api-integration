1417554d659471bcbc408972be79f85436fe36d7
Valter Rosa <valter@valterosa.com>
2025-05-01 01:07:40 +0100

    Missing 'root' route file

---
+++ remix.config.js
@@ -0,0 +1,8 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  appDirectory: "elektro3-api-module/app",
+  assetsBuildDirectory: "elektro3-api-module/build/client",
+  publicPath: "/build/",
+  serverBuildPath: "elektro3-api-module/build/server/index.js",
+  ignoredRouteFiles: ["**/.*"],
+};
